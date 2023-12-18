2fd0d230da46e7a7c0547658845929596dc804cc
JaveedinCode <cavidgaffar1@gmail.com>
2023-12-18 23:24:34 +0400

    new

--- banka.dart
+++ banka.dart
@@ -1,18 +1,16 @@
 import 'dart:io';
 
 int choice = 0;
-void main() {
-  print('Banka xos geldiniz');
-  stdout.write('hesab no:');
+void main(List<String> args) {
+  print('banka xos geldiniz');
+  stdout.write('hesab nomrenizi daxil edin:');
   int number = int.parse(stdin.readLineSync()!);
-  stdout.write('adiniz:');
+  stdout.write('adinizi daxil edin:');
   String name = stdin.readLineSync()!;
-  stdout.write('Balans:');
+  stdout.write('balansinizi daxil edin:');
   int balance = int.parse(stdin.readLineSync()!);
-
   info(number, name, balance);
   checkchoice();
-
   if (choice == 1) {
     cashin(balance);
   } else if (choice == 2) {
@@ -22,24 +20,24 @@ void main() {
 }
 
 void info(int number, String name, int balance) {
-  print('hesab no:$number');
-  print('adiniz:$name');
-  print('balansiniz:$balance');
+  print('Hesab nomreniz:$number');
+  print('Adiniz:$name');
+  print('Balansiniz:$balance');
 }
 
 void cashin(int balance) {
-  stdout.write('daxil edilecek mebleg:');
+  stdout.write('daxil etmek istediyniz meblegi daxil edin:');
   int money = int.parse(stdin.readLineSync()!);
   print('Balans:${balance + money}');
 }
 
 void cashout(int balance) {
-  stdout.write('cixarlacaq mebleg:');
+  stdout.write('cixarmaq istedyiniz meblegi daxil edin');
   int money = int.parse(stdin.readLineSync()!);
   print('Balans:${balance - money}');
 }
 
 void checkchoice() {
-  stdout.writeln('emeliyati secin:\n 1-medaxil\n 2-mexaric');
+  stdout.writeln('Ne etmek isteyirsiniz\n 1-Medaxil\n 2-Mexaric');
   choice = int.parse(stdin.readLineSync()!);
 }
